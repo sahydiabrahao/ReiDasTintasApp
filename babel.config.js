020e201c72c4d700fc2a76de79348b1873f2d8b9
sahydi <sahydi@protonmail.com>
2024-07-21 20:13:17 -0400

    refactor: add initial structure and configure absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
