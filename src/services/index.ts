dece1ebff235abf085967d3828107afff4b10d0e
sahydi <sahydi@protonmail.com>
2024-08-02 18:35:29 -0400

    refactor: update Databse location

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,6 @@ export * from './toast/useToast';
 export * from './toast/ToastProvider';
 export * from './toast/types';
 
-export * from './database/useDatabase';
-export * from './database/databaseProvider';
-export * from './database/types';
+export * from '../database/useDatabase';
+export * from '../database/databaseProvider';
+export * from '../database/types';
