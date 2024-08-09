615d3c3b09067cb5d24668daae5f9ab2b7c59be4
sahydi <sahydi@protonmail.com>
2024-08-08 20:53:25 -0400

    refactor: delete CategoryScreen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,7 +3,6 @@ export * from './components/ScreenContainer';
 export * from './app/HomeScreen';
 export * from './app/CartScreen';
 
-export * from './app/CategoryScreen';
 export * from './auth/SettingsScreen';
 export * from './app/SearchScreen';
 export * from './app/ContactScreen';
